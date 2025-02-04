1fb62190ec16182465fc9927bea61287c4179d91
belbaz <76886674+belbaz@users.noreply.github.com>
2025-02-04 15:57:23 +0100

    new link with database supabase

---
+++ pages/api/supabase.js
@@ -0,0 +1,35 @@
+// pages/api/supabase.js
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+// Client Supabase
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default async function handler(req, res) {
+    try {
+        // Vérifiez si la méthode HTTP est correcte (par exemple, GET)
+        if (req.method !== 'GET') {
+            return res.status(405).json({ error: 'Méthode non autorisée' });
+        }
+
+        // Appel à Supabase pour récupérer les données de la table "devWebTEST"
+        const { data: devWebTEST, error } = await supabase
+            .from('devWebTEST')
+            .select('*');
+
+        // Vérifiez si une erreur survient lors de la requête
+        if (error) {
+            console.error('Erreur Supabase :', error);
+            return res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+        }
+
+        // Renvoi des données dans la réponse HTTP
+        // console.log(devWebTEST);
+        return res.status(200).json(devWebTEST[0].nom);
+    } catch (err) {
+        // Gestion des erreurs imprévues
+        console.error('Erreur interne du serveur :', err);
+        return res.status(500).json({ error: 'Erreur interne du serveur' });
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,21 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 
 export default function home() {
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('. . .');
+    const [messageSupabase, setMessageSupabase] = useState('. . .');
 
     useEffect(() => {
         // Appel de l'API pour obtenir le message
         fetch('/api/hello')
             .then(response => response.json())
             .then(data => setMessage(data.message));
+
+        // Appel de l'API Supabase
+        fetch('/api/supabase')
+            .then((response) => response.json())
+            .then((data) => {
+                setMessageSupabase(data);
+            });
     }, []);
 
     return (
@@ -18,6 +26,7 @@ export default function home() {
             <main>
                 <h1>Bienvenue sur dev web !</h1>
                 <p>{message}</p>
+                <p>{messageSupabase}</p>
             </main>
             <Footer/>
         </div>
