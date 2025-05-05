b33fa233a4de2b76c4ba7740e9dd9e0bcafb5f09
belbaz <76886674+belbaz@users.noreply.github.com>
2025-05-05 15:42:08 +0200

    redeploy test with 404 page

---
+++ app/pages/404.js
@@ -0,0 +1,9 @@
+// pages/404.js
+export default function Custom404() {
+    return (
+        <div>
+            <h1>404 - Page Non Trouvée</h1>
+            <p>Désolé, la page que vous cherchez n'existe pas.</p>
+        </div>
+    )
+}
